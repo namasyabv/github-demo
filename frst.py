254112ce38082f84f6fdc5f8143e8ef7901a6f40
namasyabv <namasya_v@persistent.com>
2021-03-22 16:35:22 +0530

    function headers added.

--- frst.py
+++ frst.py
@@ -1,8 +1,13 @@
+
+#add imlemenatation
 def add(x,y):
     pass
+# subtract imlemenatation
 def subtract(x,y):
     pass
+# multiply imlemenatation
 def multiply(x,y):
     pass
+#divide imlemenatation
 def divide(x,y):
     pass
